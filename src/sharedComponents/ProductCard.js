9eeaf0a375e9e6c415fa645eb3029b73aa09cb61
DarnellS <Darnellsylvain@gmail.com>
2020-08-19 21:59:25 +0100

    added links to productpage

--- src/sharedComponents/ProductCard.js
+++ src/sharedComponents/ProductCard.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
 import blackSofaImage from '../Assets/Images/pexels-pixabay-279719.jpg'
+import { Link } from 'react-router-dom';
 
 const ProductCard = ({img}) => {
     return ( 
     <div className="card">
-        <div className="card-image">
-            <img src={blackSofaImage} alt=""/>
-        </div>
-        <p>STRANDON</p>
-        <p>£1533</p>
+        <Link to="/productpage">
+            <div className="card-image">
+                <img src={blackSofaImage} alt=""/>
+            </div>
+            <p>STRANDON</p>
+            <p>£1533</p>
+        </Link>
+
         <div className="colorCircle-container">
             <div className="colorCircle" style={{background: "#324A5F"}}></div>
             <div className="colorCircle" style={{background: "#1B2A41"}}></div>
